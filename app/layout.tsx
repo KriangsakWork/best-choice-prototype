a4ace8f851b5db1c5f040dbfc79b307519e1400b
KriangsakWork <Kriangsaklive@gmail.com>
2026-07-27 18:06:42 +0700

    Set iOS browser theme color to white

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,7 +36,7 @@ export const viewport: Viewport = {
   initialScale: 1,
   maximumScale: 1,
   viewportFit: "cover",
-  themeColor: "#fefcfb"
+  themeColor: "#ffffff"
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
